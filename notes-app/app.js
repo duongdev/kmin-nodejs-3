d8d1f836a0eb130b984c03968b8acf8b95587562
Dương Đỗ <dustin.do95@gmail.com>
2020-09-29 20:51:55 +0700

    Add: Save note list to notes.json

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,30 @@
 const yargs = require("yargs");
+const fs = require("fs");
 
 yargs.version("1.1.0");
+
+const fileName = "notes.json";
+
+// notes.json
+
+// Loads existing notes from notes.json
+const loadNotes = function () {
+  try {
+    const notesJson = fs.readFileSync(fileName, { encoding: "utf-8" });
+
+    const notesArr = JSON.parse(notesJson);
+    return notesArr;
+  } catch (error) {
+    return [];
+  }
+};
+
+// Writes note list to notes.json
+const saveNotes = function (notes) {
+  const notesJson = JSON.stringify(notes);
+
+  fs.writeFileSync(fileName, notesJson);
+};
 
 yargs.command({
   command: "add",
@@ -18,13 +42,28 @@ yargs.command({
     },
   },
   handler: function (args) {
-    // destructuring
-    const { title, description } = args;
-    // const title = args.title;
-    // const description = args.description;
+    /** destructuring
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+     * Same to:
+     * const title = args.title;
+     * const body = args.body;
+     */
+    const { title, body } = args;
 
-    console.log("Adding a new note");
-    console.log("Title:", title);
-    console.log("Description:", description);
+    /** Same to:
+     * const note = { title: title, body: body }
+     */
+    const note = { title, body };
+
+    // Loads existing notes from notes.json
+    const notes = loadNotes();
+
+    // Adds the new note to the existing list.
+    // numbers = [1,2]
+    // numbers.push(3) => [1,2,3]
+    notes.push(note);
+
+    // Saves the new list to notes.json
+    saveNotes(notes);
   },
 }).argv;
