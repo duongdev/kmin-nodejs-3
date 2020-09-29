bb39fc1aec34162e435dc0395f8394b50ecd45be
Dương Đỗ <dustin.do95@gmail.com>
2020-09-29 19:12:00 +0700

    Add nodemon & scripts

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-// Node modules
-const fs = require("fs");
 const chalk = require("chalk");
-const utils = require("./utils");
 
-console.log(chalk.green("Hello Kmin"), chalk.yellowBright.bgBlue("2020"));
-
-function writeContent(outputDir, fileName, content) {
-  const filePath = `${outputDir}/${fileName}`;
-
-  utils.checkExistsAndCreate(outputDir);
-
-  fs.writeFileSync(filePath, content);
-
-  console.log(`Written "${content}" to "${filePath}"`);
-}
-
-// writeContent("output", "text.txt", "this is a text");
-// utils.deleteFile("output/text.txt");
+console.log(
+  chalk.red.inverse("Hello Kmin!!"),
+  chalk.yellowBright.bgBlue("2020")
+);
